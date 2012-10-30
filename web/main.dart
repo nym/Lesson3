0f744a22fe64278743863443bc4db06aa458a901
Don <don.j.olmstead@gmail.com>
2012-10-29 22:36:41 -0700

    Adding in changes from lesson 2 and changes due to post M1 libraries.

--- web/main.dart
+++ web/main.dart
@@ -16,18 +16,21 @@ import 'package:vector_math/vector_math_browser.dart';
 // Source files
 //---------------------------------------------------------------------
 
+part 'texture_dialog.dart';
 part 'application/frame_counter.dart';
 part 'application/game.dart';
 
 /// The [FrameCounter] associated with the application
 FrameCounter _counter;
+/// The [TextureDialog] associated with the application
+TextureDialog _textureDialog;
 
 /**
  * Update function for the application.
  *
  * The current [time] is passed in.
  */
-void _onUpdate(int time)
+void _onUpdate(double time)
 {
   _counter.update(time);
   Game.onUpdate(time);
@@ -38,6 +41,68 @@ void _onUpdate(int time)
 }
 
 /**
+ * Opens the texture dialog.
+ */
+void _openTextureDialog(_)
+{
+  _textureDialog.show();
+}
+
+/**
+ * Callback for when the texture is changed.
+ */
+void _onTextureChange(String value)
+{
+  Game instance = Game.instance;
+
+  instance.texture = value;
+}
+
+/**
+ * Callback for when the model is changed.
+ */
+void _onModelChange(String value)
+{
+  Game instance = Game.instance;
+
+  instance.mesh = value;
+}
+
+/**
+ * Initializes the model buttons.
+ */
+void _initModelButtons()
+{
+  DivElement cubeMesh = document.query('#cube_button') as DivElement;
+  assert(cubeMesh != null);
+
+  cubeMesh.on.click.add((_) {
+    _onModelChange('/meshes/cube.mesh');
+  });
+
+  DivElement sphereMesh = document.query('#sphere_button') as DivElement;
+  assert(sphereMesh != null);
+
+  sphereMesh.on.click.add((_) {
+    _onModelChange('/meshes/sphere.mesh');
+  });
+
+  DivElement planeMesh = document.query('#plane_button') as DivElement;
+  assert(planeMesh != null);
+
+  planeMesh.on.click.add((_) {
+    _onModelChange('/meshes/plane.mesh');
+  });
+
+  DivElement cylinderMesh = document.query('#cylinder_button') as DivElement;
+  assert(cylinderMesh != null);
+
+  cylinderMesh.on.click.add((_) {
+    _onModelChange('/meshes/cylinder.mesh');
+  });
+}
+
+/**
  * Main entrypoint for every Dart application.
  */
 void main()
@@ -45,6 +110,15 @@ void main()
   // Initialize the WebGL side
   Game.onInitialize();
   _counter = new FrameCounter('#frame_counter');
+
+  // Initialize the UI side
+  _textureDialog = new TextureDialog();
+  _textureDialog.submitCallback = _onTextureChange;
+
+  DivElement replaceButton = document.query('#replace') as DivElement;
+  replaceButton.on.click.add(_openTextureDialog);
+
+  _initModelButtons();
 
   // Start the animation loop
   window.requestAnimationFrame(_onUpdate);
